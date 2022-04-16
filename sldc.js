0666ce7ecd3dc9899592f36cdc1cd2ae9f75805e
Adrie <adriebuendia@icloud.com>
2022-04-16 00:35:15 -0400

    First Checkpoint

---
+++ sldc.js
@@ -0,0 +1,25 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
+const analysisOfRequirements = 'defining user expectations for the new software being built or modified'
+const Design = 'developers and technical architects start the high-level design of the software and system to be able to deliver each requirement and defines all the components that needs to be developed'
+const Implementation = 'developers start coding according to the requirements and the design discussed in previous phases'
+const testingAndIntegration = 'testers test the system against the requirements and intend to find any defects and make sure it behaves as expected'
+const maintain = 'a team looks out for the software and if any issues arise they fix it.'
